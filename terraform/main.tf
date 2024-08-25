d82074e69116f0ad3315f918189759c1b607c77e
Raj Sah <93470062+y-26@users.noreply.github.com>
2024-08-25 10:00:41 +0000

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,68 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
+  location = "East US"
+}
+
+# Backend App Service Plan (Windows)
+resource "azurerm_app_service_plan" "backend_plan" {
+  name                = "backend-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Windows"
+  reserved            = false
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+# Backend App Service
+resource "azurerm_app_service" "backend_app" {
+  name                = "backend-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.backend_plan.id
+
+  site_config {
+    python_version = "3.10"
+  }
+}
+
+# Frontend App Service Plan (Linux)
+resource "azurerm_app_service_plan" "frontend_plan" {
+  name                = "frontend-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+# Frontend App Service
+resource "azurerm_app_service" "frontend_app" {
+  name                = "frontend-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.frontend_plan.id
+
+  site_config {
+    linux_fx_version = "NODE|18-lts"
+  }
+}
+
+output "backend_app_url" {
+  value = azurerm_app_service.backend_app.default_site_hostname
+}
+
+output "frontend_app_url" {
+  value = azurerm_app_service.frontend_app.default_site_hostname
+}
